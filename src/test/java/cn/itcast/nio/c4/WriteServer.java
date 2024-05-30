4e05546f662c44aa76ffddeb38fec28234a4da9f
chencl <2394527308@qq.com>
2024-05-30 17:46:45 +0800

    feat: 处理消息边界

--- src/test/java/cn/itcast/nio/c4/WriteServer.java
+++ src/test/java/cn/itcast/nio/c4/WriteServer.java
@@ -30,6 +30,7 @@ public class WriteServer {
                     for (int i = 0; i < 5000000; i++) {
                         sb.append("a");
                     }
+                    //为每一个客户端分配一个 ByteBuffer
                     ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
 
                     // 2. 返回值代表实际写入的字节数
@@ -45,8 +46,9 @@ public class WriteServer {
                         sckey.attach(buffer);
                     }
                 } else if (key.isWritable()) {
+                    //获取key上关联的附件
                     ByteBuffer buffer = (ByteBuffer) key.attachment();
-                    SocketChannel sc = (SocketChannel) key.channel();
+                    SocketChannel sc = (SocketChannel) key.channel();//拿到触发时间的channel
                     int write = sc.write(buffer);
                     System.out.println(write);
                     // 6. 清理操作
