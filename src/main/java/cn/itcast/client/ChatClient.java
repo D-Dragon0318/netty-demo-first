7cee157b41307829827e553fa962eced11ac3884
spridra <2394527308@qq.com>
2024-06-21 16:25:03 +0800

    feat:聊天室登录功能

--- src/main/java/cn/itcast/client/ChatClient.java
+++ src/main/java/cn/itcast/client/ChatClient.java
@@ -1,9 +1,13 @@
 package cn.itcast.client;
 
+import cn.itcast.message.LoginRequestMessage;
+import cn.itcast.message.LoginResponseMessage;
 import cn.itcast.protocol.MessageCodecSharable;
 import cn.itcast.protocol.ProcotolFrameDecoder;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -12,12 +16,19 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Slf4j
 public class ChatClient {
     public static void main(String[] args) {
         NioEventLoopGroup group = new NioEventLoopGroup();
         LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        CountDownLatch WAIT_FOR_LOGIN = new CountDownLatch(1);
+        AtomicBoolean LOGIN = new AtomicBoolean(false);
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.channel(NioSocketChannel.class);
@@ -26,8 +37,65 @@ public class ChatClient {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new ProcotolFrameDecoder());
-                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    // ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast("client handler",new ChannelInboundHandlerAdapter(){
+
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.debug("msg: {}",msg);
+                            if (msg instanceof LoginResponseMessage){
+                                LoginResponseMessage response = (LoginResponseMessage) msg;
+                                if (response.isSuccess()){
+                                    LOGIN.set(true);
+                                }
+                                //不管成功还是失败都要唤醒 system in 线程
+                                WAIT_FOR_LOGIN.countDown();
+                            }
+                        }
+
+                        //连接建立后触发active
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            new Thread(()->{
+                                Scanner scanner = new Scanner(System.in);
+                                System.out.println("请输入用户名");
+                                String username = scanner.nextLine();
+                                System.out.println("请输入密码");
+                                String password = scanner.nextLine();
+                                //构造登录对象
+                                LoginRequestMessage loginRequestMessage = new LoginRequestMessage(username, password);
+                                ctx.writeAndFlush(loginRequestMessage);
+                                System.out.println("等待后续操作");
+                                //等待进一步输入
+                                /* try {
+                                    System.in.read();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                } */
+                                try {
+                                    WAIT_FOR_LOGIN.await();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                if (!LOGIN.get()) {
+                                    ctx.channel().close();
+                                    return;
+                                }
+                                while (true){
+                                    System.out.println("==================================");
+                                    System.out.println("send [username] [content]");
+                                    System.out.println("gsend [group name] [content]");
+                                    System.out.println("gcreate [group name] [m1,m2,m3...]");
+                                    System.out.println("gmembers [group name]");
+                                    System.out.println("gjoin [group name]");
+                                    System.out.println("gquit [group name]");
+                                    System.out.println("quit");
+                                    System.out.println("==================================");
+                                }
+                            },"system in").start();
+                        }
+                    });
                 }
             });
             Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
@@ -35,6 +103,7 @@ public class ChatClient {
         } catch (Exception e) {
             log.error("client error", e);
         } finally {
+            //正常关闭
             group.shutdownGracefully();
         }
     }
